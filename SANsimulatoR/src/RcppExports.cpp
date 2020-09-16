04332a55fb5bc9d894008f4f883cbc5d91a6a488
Florian G. Pflug <florian.pflug@univie.ac.at>
2020-09-16 22:49:59 +0200

    Re-generated RcppExports.cpp, was forgotten after changing parameters to be const

--- SANsimulatoR/src/RcppExports.cpp
+++ SANsimulatoR/src/RcppExports.cpp
@@ -6,19 +6,19 @@
 using namespace Rcpp;
 
 // san_timediscrete_c
-List san_timediscrete_c(List C_init, NumericVector p_S, NumericVector p_0, NumericVector p_R, NumericVector p_A, NumericVector p_N, NumericVector p_D, IntegerVector steps);
+List san_timediscrete_c(const List C_init, const NumericVector p_S, const NumericVector p_0, const NumericVector p_R, const NumericVector p_A, const NumericVector p_N, const NumericVector p_D, const IntegerVector steps);
 RcppExport SEXP _SANsimulatoR_san_timediscrete_c(SEXP C_initSEXP, SEXP p_SSEXP, SEXP p_0SEXP, SEXP p_RSEXP, SEXP p_ASEXP, SEXP p_NSEXP, SEXP p_DSEXP, SEXP stepsSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< List >::type C_init(C_initSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_S(p_SSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_0(p_0SEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_R(p_RSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_A(p_ASEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_N(p_NSEXP);
-    Rcpp::traits::input_parameter< NumericVector >::type p_D(p_DSEXP);
-    Rcpp::traits::input_parameter< IntegerVector >::type steps(stepsSEXP);
+    Rcpp::traits::input_parameter< const List >::type C_init(C_initSEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_S(p_SSEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_0(p_0SEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_R(p_RSEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_A(p_ASEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_N(p_NSEXP);
+    Rcpp::traits::input_parameter< const NumericVector >::type p_D(p_DSEXP);
+    Rcpp::traits::input_parameter< const IntegerVector >::type steps(stepsSEXP);
     rcpp_result_gen = Rcpp::wrap(san_timediscrete_c(C_init, p_S, p_0, p_R, p_A, p_N, p_D, steps));
     return rcpp_result_gen;
 END_RCPP
