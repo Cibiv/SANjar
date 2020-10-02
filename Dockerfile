a3d45c09bf1a2523872447c1149f81d476f96a66
Florian G. Pflug <florian.pflug@univie.ac.at>
2020-10-02 15:23:58 +0200

    Base SANModelExplorer's Dockerfile on cibiv-shiny-base.
    Also extended docker-publish to avoid re-tagging the same revision multiple times

--- Dockerfile
+++ Dockerfile
@@ -1,50 +1,11 @@
-FROM openanalytics/r-base:4.0.2
+FROM cibiv-shiny-base-r40:2020-10-02
 
 MAINTAINER Florian G. Pflug <florian.pflug@univie.ac.at>
 
 VOLUME /state
 
-# system libraries of general use
-RUN apt-get update && apt-get install -y \
-    sudo \
-    pandoc \
-    pandoc-citeproc \
-    libcurl4-gnutls-dev \
-    libcairo2-dev \
-    libxt-dev \
-    libssl-dev \
-    libssh2-1-dev \
-    libssl1.0.0 \
-    libxml2-dev
-
-# Install inline package and anything that might be usefull or is a common dependency
-RUN R -e "install.packages(c( \
-        'inline', 'Rcpp', 'RcppParallel', 'RcppEigen', 'RcppArmadillo', 'BH' \
-      ), repos='https://cloud.r-project.org/')"
-
-# Install basic R packages
-RUN R -e "install.packages(c( \
-        'data.table', 'MASS', 'VGAM', 'Deriv', 'lamW' \
-      ), repos='https://cloud.r-project.org/')"
-
-# Install R packages data.table, inline, shiny, ggplot2 and extensions
-# Note that Hmisc is listed here because it depends on ggplot2
-RUN R -e "install.packages(c( \
-        'shinycssloaders', 'DT', 'rhandsontable', \
-        'ggplot2', 'scales', 'cowplot', 'gridExtra', 'Hmisc' \
-      ), repos='https://cloud.r-project.org/')"
-
 # Install R packages not on CRAN via devtools
-RUN R -e "install.packages(c( \
-        'devtools' \
-      ), repos='https://cloud.r-project.org/')" && \
-    R -e "library(devtools); install_github('schaffman5/shinyTree', ref='e41e200437d828faf203c22b98f9f6d6cac58206')" && \
-    R -e "library(devtools); install_github('Cibiv/gwpcR', ref='v0.9.10')"
-
-# Install additional packages
-RUN R -e "install.packages(c( \
-        'bsplus', 'dqrng', 'readr' \
-    ), repos='https://cloud.r-project.org/')"
+RUN R -e "library(devtools); install_github('Cibiv/gwpcR', ref='v0.9.10')"
 
 # R seettings
 COPY Rprofile.site /usr/lib/R/etc/
--- docker-publish.sh
+++ docker-publish.sh
@@ -4,6 +4,11 @@ function die() {
 	exit 1
 }
 
+
+DOCKER_REGISTRY="minsky:5000"
+CONTAINER_NAME="sanmodelexplorer"
+APPLICATION_SERVER="minsky"
+
 if [ "$(git symbolic-ref --short HEAD)" != "master" ]; then
 	echo "Currently checked out branch must be 'master'" >&2
         exit 1
@@ -14,21 +19,27 @@ if [ $(git status --porcelain | wc -l) != 0 ]; then
 	exit 1
 fi
 
+echo "*** Fetching tags"
 git fetch --tags || die "Unable to fetch tags"
-rev=$[ $(git tag | sed -n 's/^rev\([0-9]*\)$/\1/p' | sort -n | tail -n1) + 1 ]
-echo "*** Publishing revision $rev"
 
-echo "*** Tagging revision"
-git tag rev$rev || die "Unable to tag current version as rev$rev"
-git push origin master rev$rev
+rev=$(git tag --points-at HEAD | sed -n 's/^rev\([0-9]*\)$/\1/p')
+if [ "$rev" == "" ]; then
+	rev=$[ $(git tag | sed -n 's/^rev\([0-9]*\)$/\1/p' | sort -n | tail -n1) + 1 ]
+	echo "*** Publishing new revision $rev"
+	echo "*** Tagging HEAD as rev$rev and pushing tag"
+	git tag rev$rev || die "Unable to tag current version as rev$rev"
+	git push origin master rev$rev || die "Unable to push rev$rev to origin"
+else
+	echo "*** Publishing existing revision $rev"
+fi
 
-img_tag="sanmodelexplorer:r$rev"
+img_tag="$CONTAINER_NAME:r$rev"
 echo "*** Building image $img_tag"
 docker build --build-arg CONTAINER_REVISION="rev$rev" -t "$img_tag" "$(dirname $BASH_SOURCE)" || exit 1
 img_hash="$(docker images -q "$img_tag")"
 
-publish_rev_tag="minsky:5000/$img_tag"
-publish_latest_tag="minsky:5000/sanmodelexplorer:latest"
+publish_rev_tag="$DOCKER_REGISTRY/$img_tag"
+publish_latest_tag="$DOCKER_REGISTRY/$CONTAINER_NAME:latest"
 echo "*** Tagging image $img_tag (hash $img_hash) for publication as $publish_rev_tag and as $publish_latest_tag"
 docker tag "$img_tag" "$publish_rev_tag" || exit 1
 docker tag "$img_tag" "$publish_latest_tag" || exit 1
@@ -37,5 +48,5 @@ echo "*** Publishing $publish_rev_tag"
 docker push "$publish_rev_tag" || exit 1
 docker push "$publish_latest_tag" || exit 1
 
-echo "*** Pulling new image on minsky, shinyproxy should use it automatically"
-ssh minsky docker pull --quiet "$publish_latest_tag" || exit 1
+echo "*** Pulling new image on $APPLICATION_SERVER, shinyproxy should use it automatically"
+ssh "$APPLICATION_SERVER" docker pull --quiet "$publish_latest_tag" || exit 1
