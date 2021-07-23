7cf40359a683f13e5bf5507018268acfa895fa58
Florian G. Pflug <fgp@phlo.org>
2021-07-23 14:19:09 +0200

    Turned LT47 and the FACS and area-based organoid sizes into R package data sets
    Moved the raw data to data-raw, and combined the data pre-processing into
    the script data/lt47.R which generates data/h9_organoidsizes.RData and
    data/lt47.RData.

---
+++ data-raw/lt47.R
@@ -0,0 +1,92 @@
+library(data.table)
+library(gwpcR)
+
+message("*** Importing FACS-based organoid sizes from h9_organoidsizes_facs.tsv")
+h9_organoidsizes_facs <- data.table(read.table("data-raw/h9_organoidsizes_facs.tsv", header=TRUE, sep="\t"))
+h9_organoidsizes_facs <- h9_organoidsizes_facs[, list(
+  day, source="facs", rep, cells=count
+)]
+
+message("*** Importing area-based organoid sizes from h9_organoidsizes_area.tsv")
+h9_organoidsizes_area <- data.table(read.table("data-raw/h9_organoidsizes_area.tsv", header=TRUE, sep="\t",
+                                               check.names=FALSE))
+h9_organoidsizes_area <- h9_organoidsizes_area[, list(
+  day=as.integer(substr(`Day`, 4, 6)),
+  source="area",
+  rep=paste0("b", trimws(substr(`Batch`, 6, 8))),
+  volume=`Volume`
+)]
+
+message("*** Combinding FACS- and area-based organoid sizes")
+# Combine FACS- and area-based data based on the (interpolated) FACS count for day 10.
+# The FACS-based measurements underestimate the organoid size from day 25 onwards because
+# they fail to include dead cells, and are hence excluded.
+f9 <- mean(h9_organoidsizes_facs[day==9, log(cells)])
+f13 <- mean(h9_organoidsizes_facs[day==13, log(cells)])
+a10 <- mean(h9_organoidsizes_area[day==10, log(volume)])
+s <- exp((3/4)*f9+(1/4)*f13-a10)
+h9_organoidsizes <- rbind(
+  h9_organoidsizes_facs,
+  h9_organoidsizes_area[, list(day, source, rep, cells=round(s*volume))]
+)[((source=="facs") & (day < 25)) | (source=="area")]
+
+message("*** Saving organoid sizes to data/h9_organoidsizes.RData")
+save(h9_organoidsizes, file="data/h9_organoidsizes.RData", version=2)
+
+message("*** Importing lineage sizes from data-raw/lt47_h9_only.tsv")
+lt47_ls <- data.table(read.table("data-raw/lt47_h9_only.tsv", header=TRUE,
+                                 sep="\t", quote="\"", stringsAsFactors=FALSE))
+
+message("*** Converting lineage sizes into data.table")
+lt47_ls <- lt47_ls[, {
+  #    m <- regexec("^(\\d+)-H9-day(\\d\\d)-(\\d)+(-PCR\\d)?$", Sample)
+  m <- regexec("^H9-day(\\d\\d)-(\\d+)$", Sample)
+  stopifnot(all(sapply(m, `[`, 1) > 0))
+  sid <- as.integer(sapply(regmatches(Sample, m), `[`, 3))
+  day <- as.integer(sapply(regmatches(Sample, m), `[`, 2))
+  list(sample=as.factor(Sample), sid=sid, day=day, reads=as.integer(nLineages))
+}]
+last_sid <- 0L
+lt47_ls[, sid := { last_sid <<- last_sid + 1L }, by=c("day", "sid")]
+setkey(lt47_ls, sid)
+
+message("*** Removing low-quality samples")
+lt47_ls <- lt47_ls[!(sid %in% c(15, 16))]
+
+message("*** Translating lineage sizes from #reads to #cells")
+reads_per_cell <- lt47_ls[, {
+  f <- density(log10(reads))
+  list(reads.per.cell=10^(f$x[which.max(f$y)]))
+}, keyby=.(sid, day)]
+
+lt47_ls_cells <- lt47_ls[reads_per_cell, {
+  list(day=day,
+       cells=reads/reads.per.cell)
+}, keyby=.EACHI]
+
+message("*** Estimating PCR and sequencing parameters")
+sequencing <- lt47_ls[, {
+  # Estimate parameters for each sample
+  p <- .SD[, list(library_size=sum(reads),
+                  phantom_threshold=min(reads)),
+           , by="sid"]
+  # Compute median across all samples for a specific day
+  p[, list(library_size=signif(round(median(library_size)), 2),
+           phantom_threshold=round(median(phantom_threshold))) ]
+}, by=.(day)]
+# Compute PCR efficiency from day-0 samples
+sequencing[, pcr_efficiency := {
+  lt47_ls[day==0][, {
+    gwpcrpois.est(reads, threshold=min(reads), molecules=1)
+  }, by="sid"][, list(
+    pcr_efficiency=signif(median(efficiency), 2)
+  )]
+}]
+
+message("*** Constructing lt47 dataset object")
+lt47 <- list(organoidsizes=h9_organoidsizes,
+             lineagesizes=lt47_ls_cells,
+             sequencing=sequencing)
+
+message("*** Saving lt47 to data/lt47.RData")
+save(lt47, file="data/lt47.RData", version=2)
--- data/organoidsizes.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-
-# Load cell count data
-message("*** Loading organoidsizes.tab")
-ORGANOIDSIZES <- data.table(read.table("organoidsizes.tab", header=TRUE, colClasses=c(
-  day="integer", source="factor", rep="factor", count="integer", hq="logical")))
-
-message("*** Saving as organoidsizes.rd")
-save(ORGANOIDSIZES, file="organoidsizes.rd", version=2)
