b695950db2c42281d932419d1de7962fb1d8ce81
Florian G. Pflug <fgp@phlo.org>
2021-08-16 14:29:13 +0200

    Handle NAN and infinit means values gracefully

--- R/tpois.R
+++ R/tpois.R
@@ -8,8 +8,10 @@ tpois.lambda.eq <- deriv(expression((m - l/(1-exp(-l)))^2), namevec=c("l"),
 #' @param mean the desired mean of the zero-truncated Poisson distribution
 #' @return the lambda Parameter of the truncated Poisson distribution
 tpois.lambda <- function(mean) {
-  if (mean <= 1)
+  if (is.na(mean) || (mean <= 1))
     return(NA_real_)
+  if (is.infinite(mean))
+    return(Inf)
   r <- nlm(tpois.lambda.eq, p=mean, m=mean)
   return(r$estimate)
 }
