1eb39f7a934f54d5ed0552d1bdef12a5756e966f
Florian G. Pflug <fgp@phlo.org>
2021-08-17 17:31:58 +0200

    Added missing ggplot2:: qualifiers

--- R/plot.R
+++ R/plot.R
@@ -58,7 +58,7 @@ plot.SANPosterior <- function(posterior, data, params=NULL, ll=NULL, plotlist=FA
     ggplot2::geom_errorbar(data=posterior$data$cc, ggplot2::aes(x=day, ymin=10^(logmean-logsd), ymax=10^(logmean+logsd))) +
     ggplot2::geom_line(data=ll.cc, ggplot2::aes(x=day, y=pmax(value, posterior$arguments$min.size),
                                                 color=ll_cc, group=index)) +
-    make_scale_log10(scale_y_log10) +
+    make_scale_log10(ggplot2::scale_y_log10) +
     ggplot2::scale_color_gradient2(name=expression(log~L), trans=log_ll,
                                    low="red", mid="green", midpoint=midpoint(ll.cc$ll_cc), high="blue") +
     ggplot2::labs(y="cells")
@@ -78,14 +78,14 @@ plot.SANPosterior <- function(posterior, data, params=NULL, ll=NULL, plotlist=FA
     }]
     
     # Plot
-    ggplot() +
+    ggplot2::ggplot() +
       ggplot2::geom_errorbar(data=posterior$data$rs[day==rs_day],
                              ggplot2::aes(x=rank, ymin=10^(logmean-logsd), ymax=10^(logmean+logsd))) +
       ggplot2::geom_line(data=ll.rs, ggplot2::aes(x=rank, y=pmax(value, posterior$arguments$min.size),
                                                   color=ll_rs, group=index)) +
-      annotate("text", x=Inf, y=Inf, hjust=1, vjust=1, col="black", label=paste0("day ", rs_day)) +
-      make_scale_log10(scale_x_log10) +
-      make_scale_log10(scale_y_log10) +
+      ggplot2::annotate("text", x=Inf, y=Inf, hjust=1, vjust=1, col="black", label=paste0("day ", rs_day)) +
+      make_scale_log10(ggplot2::scale_x_log10) +
+      make_scale_log10(ggplot2::scale_y_log10) +
       ggplot2::scale_color_gradient2(name=expression(log~L), trans=log_ll,
                                      low="red", mid="green", midpoint=midpoint(ll.rs$ll_rs), high="blue") +
       ggplot2::labs(y=posterior$data$unit)
